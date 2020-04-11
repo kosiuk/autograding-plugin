05b7c4bd2b69860159435d9209243146bf1662e6
Lion Kosiuk <lik7907@thi.de>
2020-04-11 12:45:25 +0200

    jgjfjg jf Please enter the commit message for your changes. Lines starting

--- pipeline/script.groovy
+++ pipeline/script.groovy
@@ -5,9 +5,11 @@ pipeline {
         stage('autograding') {
             steps {
                 publishCoverage adapters: [jacocoAdapter('pipeline/jacoco.xml')], sourceFileResolver: sourceFiles('NEVER_STORE')
+                sh label: '', script: 'touch pipeline/junit-testResults/*'
                 junit testResults: 'pipeline/junit-testResults/TEST-*.xml'
                 step([$class: 'PitPublisher', mutationStatsFile: 'pipeline/mutations.xml'])
                 recordIssues(tools: [checkStyle()])
+                autoGrade('{"analysis":{"maxScore":100,"errorImpact":-10,"highImpact":-5,"normalImpact":-2,"lowImpact":-1}, "tests":{"maxScore":100,"passedImpact":1,"failureImpact":-5,"skippedImpact":-1}, "coverage":{"maxScore":100,"coveredImpact":1,"missedImpact":-1}, "pit":{"maxScore":100,"detectedImpact":1,"undetectedImpact":-1,"ratioImpact":0}}')
             }
         }
     }
