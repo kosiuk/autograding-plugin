83ad5fe5af18e82f40650bfea93ba6f818f653b7
Lion Kosiuk <lik7907@thi.de>
2020-04-11 12:31:29 +0200

    hggff

--- pipeline/script.groovy
+++ pipeline/script.groovy
@@ -5,7 +5,7 @@ pipeline {
         stage('autograding') {
             steps {
                 publishCoverage adapters: [jacocoAdapter('pipeline/jacoco.xml')], sourceFileResolver: sourceFiles('NEVER_STORE')
-                junit testResults: '**/target/*-reports/TEST-*.xml'
+                junit testResults: 'target/*-reports/TEST-*.xml'
                 step([$class: 'PitPublisher', mutationStatsFile: 'pipeline/mutations.xml'])
                 recordIssues(tools: [checkStyle()])
             }
